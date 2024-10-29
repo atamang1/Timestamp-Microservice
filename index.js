25b78db9497e534e919e076b7cd24c6a21763453
Ashok Tamang <ashoktamang1108@gmail.com>
2024-10-29 12:25:54 -0400

    feat: api for current and specific unix and utc timestamp

--- index.js
+++ index.js
@@ -20,8 +20,27 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get('/api/:date?', function (req, res) {
+// let unix = Date.now(); //get unix time 
+  //res.json({unix: `${unix}`, utc: `${utc}`});
+  let inputDate =  new Date(req.params.date)
+  let utc; 
+  let unix; 
+
+  if(!isNaN(inputDate)){
+    utc = inputDate.toUTCString();
+    unix = inputDate.valueOf();
+  }else 
+  {
+    unix = parseInt(req.params.date, 10); 
+    if(isNaN(unix.getTime())){
+      return res.json({error: "Invalid Date"}); 
+    }
+    utc = new Date(unix).toUTCString();
+  }
+  
+  res.json({unix: unix, utc: `${utc}`});
+
 });
 
 
